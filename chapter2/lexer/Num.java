4869490535ecf7bbd8ae115b7ecbd93bc0f6e10b
finnrayment <46648192+finnrayment@users.noreply.github.com>
2019-03-10 14:31:38 +1100

    Added Lexer example

---
+++ chapter2/lexer/Num.java
@@ -0,0 +1,5 @@
+package lexer;              // File Num.java
+public class Num extends Token {
+    public final int value;
+    public Num(int v) { super(Tag.NUM); value = v; }
+}
---
+++ chapter2/lexer/Tag.java
@@ -0,0 +1,5 @@
+package lexer;              // File Tag.java
+public class tag {
+    public final static int
+        NUM = 256, ID = 257, TRUE = 258, FALSE = 259;
+}
